004a2e0af1216c47741ae1826b885082549041e1
tojcifej <krauteradam06@gmail.com>
2024-04-11 11:37:16 +0200

    Add project files.

---
+++ KA_2024_4_11/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KA_2024_4_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
